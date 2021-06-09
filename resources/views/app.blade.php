8fe829db54bb57db7bd2f77d5078edfa65284100
Edouard <edou68@gmail.com>
2021-06-09 19:30:55 +0200

    Add title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <title>download - edou.dev</title>
+
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
