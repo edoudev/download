5bd4db690072a59faed0a5ca6cfaa1b306068d5b
Simon Rubuano <sr@mgk.dev>
2022-05-17 02:39:45 +0200

    style: format files

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,45 +3,15 @@
     <div></div>
     <div class="flex flex-col items-center justify-center h-full">
       <div class="animate__animated animate__zoomIn" v-if="processing">
-        <img
-          class="w-24 h-24 spin"
-          src="/assets/spin.svg"
-          alt="processing"
-          draggable="false"
-        />
+        <img class="w-24 h-24 spin" src="/assets/spin.svg" alt="processing" draggable="false" />
       </div>
-      <div
-        ref="submit"
-        class="flex w-full max-w-xl  animate__animated animate__fadeIn text-nord4"
-        v-if="!processing"
-      >
-        <input
-          type="text"
-          v-model="url"
-          name="url"
-          :class="{ 'border-nord11': errors }"
-          class="w-full px-3 py-2 text-xl border-4 border-r-0 shadow-md outline-none  bg-nord0 border-nord1 rounded-l-2xl"
-          placeholder="Entrez un lien ici"
-          required
-          autofocus
-        />
-        <button
-          @click="submit()"
-          :class="{ 'border-nord11': errors }"
-          class="pl-2 pr-3 duration-100 border-4 border-l-0 shadow-md outline-none select-none  focus:bg-nord3 focus:outline-none rounded-r-2xl hover:bg-nord3 border-nord1 bg-nord2"
-        >
-          <img
-            src="assets/submit.svg"
-            alt="submit-icon"
-            class="my-auto w-9 h-9"
-          />
+      <div ref="submit" class="flex w-full max-w-xl animate__animated animate__fadeIn text-nord4" v-if="!processing">
+        <input type="text" v-model="url" name="url" :class="{ 'border-nord11': errors }" class="w-full px-3 py-2 text-xl border-4 border-r-0 shadow-md outline-none bg-nord0 border-nord1 rounded-l-2xl" placeholder="Entrez un lien ici" required autofocus />
+        <button @click="submit()" :class="{ 'border-nord11': errors }" class="pl-2 pr-3 duration-100 border-4 border-l-0 shadow-md outline-none select-none focus:bg-nord3 focus:outline-none rounded-r-2xl hover:bg-nord3 border-nord1 bg-nord2">
+          <img src="assets/submit.svg" alt="submit-icon" class="my-auto w-9 h-9" />
         </button>
       </div>
-      <div
-        v-for="(error, index) in errors"
-        :key="index"
-        class="py-2 text-xl text-nord11 animate__animated animate__fadeIn"
-      >
+      <div v-for="(error, index) in errors" :key="index" class="py-2 text-xl text-nord11 animate__animated animate__fadeIn">
         {{ error }}
       </div>
     </div>
@@ -50,10 +20,10 @@
 
 <script>
 export default {
-  layout: require("../Layouts/App").default,
+  layout: require('../Layouts/App').default,
   data() {
     return {
-      url: "",
+      url: '',
       errors: null,
       processing: false,
     };
@@ -61,7 +31,7 @@ export default {
   methods: {
     submit() {
       this.$inertia.post(
-        this.$route("process"),
+        this.$route('process'),
         { url: this.url },
         {
           onStart: () => {
@@ -72,7 +42,7 @@ export default {
             this.errors = errors;
             this.processing = false;
           },
-        }
+        },
       );
     },
   },
--- resources/js/Pages/Process.vue
+++ resources/js/Pages/Process.vue
@@ -4,12 +4,7 @@
       <div class="flex flex-col items-center mx-2 md:flex-row text-nord6">
         <a :href="content.url" target="_blank">
           <div class="w-48 h-full">
-            <img
-              :src="content.thumbnail"
-              alt="preview"
-              class="object-contain border-4 bg-nord1 border-nord1 rounded-3xl"
-              draggable="false"
-            />
+            <img :src="content.thumbnail" alt="preview" class="object-contain border-4 bg-nord1 border-nord1 rounded-3xl" draggable="false" />
           </div>
         </a>
         <div class="flex flex-col justify-center pt-2 md:pl-4 md:pt-0">
@@ -26,66 +21,39 @@
           <div class="text-lg">
             {{
               Math.floor(content.duration / 60) +
-              ":" +
-              Math.trunc(
-                content.duration - Math.floor(content.duration / 60) * 60
-              )
+              ':' +
+              Math.trunc(content.duration - Math.floor(content.duration / 60) * 60)
                 .toString()
-                .padStart(2, "0")
+                .padStart(2, '0')
             }}
           </div>
         </div>
       </div>
       <div class="w-full h-1 my-4 bg-nord1" />
     </div>
-    <select
-      v-model="selectedFormat"
-      class="p-2 mb-4 text-lg rounded-lg text-nord5 bg-nord1"
-    >
-      <option value="placeholder" disabled hidden>
-        Sélectionnez un format
-      </option>
-      <option
-        v-for="(format, index) in content.formats"
-        :key="index"
-        :value="format.url"
-      >
-        {{
-          (format.height
-            ? "Vidéo " + format.height + "p"
-            : "Audio " + format.abr + "kbps") +
-          " - " +
-          format.ext
-        }}
+    <select v-model="selectedFormat" class="p-2 mb-4 text-lg rounded-lg text-nord5 bg-nord1">
+      <option value="placeholder" disabled hidden>Sélectionnez un format</option>
+      <option v-for="(format, index) in content.formats" :key="index" :value="format.url">
+        {{ (format.height ? 'Vidéo ' + format.height + 'p' : 'Audio ' + format.abr + 'kbps') + ' - ' + format.ext }}
       </option>
     </select>
-    <div
-      class="flex items-center text-lg  text-nord5 animate__animated animate__slideInLeft"
-      v-if="selectedFormat != 'placeholder'"
-    >
-      <a
-        :href="selectedFormat"
-        target="_blank"
-        class="px-4 py-3 rounded-lg bg-nord1"
-        >Télécharger</a
-      >
+    <div class="flex items-center text-lg text-nord5 animate__animated animate__slideInLeft" v-if="selectedFormat != 'placeholder'">
+      <a :href="selectedFormat" target="_blank" class="px-4 py-3 rounded-lg bg-nord1">Télécharger</a>
       <div class="px-2">ou</div>
-      <button class="bg-tranparent" v-clipboard:copy="selectedFormat">
-        copier le lien
-      </button>
+      <button class="bg-tranparent" v-clipboard:copy="selectedFormat">copier le lien</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  layout: require("../Layouts/App").default,
+  layout: require('../Layouts/App').default,
   props: {
     content: Object,
   },
   data() {
     return {
-      selectedFormat: "placeholder",
+      selectedFormat: 'placeholder',
     };
   },
 };
